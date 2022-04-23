ea5dac3edacc6f7b0d5e52a31412ded15d98931c
DzungDang <ddbdzung.wa@gmail.com>
2022-04-23 15:08:19 +0700

    SET identityNumber unique

--- src/models/ctv.model.js
+++ src/models/ctv.model.js
@@ -4,7 +4,7 @@ const mongooseDelete = require('mongoose-delete');
 const Ctv = new mongoose.Schema(
     {
         fullname: { type: String, required: true },
-        identityNumber: { type: String, minLength: 10, maxLength: 10, required: true },
+        identityNumber: { type: String, minLength: 10, maxLength: 10, required: true, unique: true },
         className: { type: String, enum: ['CNTT01', 'CNTT02', 'CNTT03', 'CNTT04', 'CNTT05', 'CNTT06', 'KHMT01', 'KHMT02', 'KTPM01', 'KTPM02', 'KTPM03', 'KTPM04', 'HTTT1', 'HTTT2', 'HTTT3', 'HTTT4'], required: true },
         gender: { type: String, enum: ['male', 'female', 'other'], required: true },
         email: { type: String, required: true },
